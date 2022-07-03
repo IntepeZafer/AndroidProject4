f11676915505f6e357668bdb8f7876b4ba715f89
IntepeZafer <intepezafer@gmail.com>
2022-07-03 13:02:56 +0300

    Bu benim aktivityler arasında bilgi alış verişi üzerine yaptığım bir app

--- app/src/main/java/com/example/multiplayactivity/MainActivity2.java
+++ app/src/main/java/com/example/multiplayactivity/MainActivity2.java
@@ -3,18 +3,12 @@ package com.example.multiplayactivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    TextView textView;
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-    public void channgeActivity(View view){
-
+        setContentView(R.layout.activity_main2);
     }
 }
